0f6a1c3fe7fef044f04ea2f0bcb949f654557969
Radosław Orczykowski <radek.or@gmail.com>
2024-05-19 20:05:19 +0200

    add container

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,16 @@
-import styles from './SearchForm.module.scss'
-import TextInput from '../TextInput/TextInput';
-import Button from '../Button/Button';
+import styles from "./SearchForm.module.scss";
+import TextInput from "../TextInput/TextInput";
+import Button from "../Button/Button";
 
 const SearchForm = () => {
-    return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder='Search…' />
-            <Button text='search'/>
-        </form>
-    );
-  };
+	return (
+		<form className={styles.searchForm}>
+			<TextInput placeholder="Search…" />
+			<Button>
+				<span className={'fa fa-search'} />
+			</Button>
+		</form>
+	);
+};
 
-  export default SearchForm;
+export default SearchForm;
