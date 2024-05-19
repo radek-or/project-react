323e693aff6bbff4cd070f6da1db29703a586a2e
Radosław Orczykowski <radek.or@gmail.com>
2024-05-19 19:27:45 +0200

    add new components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,17 @@
+import styles from './List.module.scss'
+import Column from '../Column/Column';
+
 const List = () => {
 	return (
-		<div>
-			<header>
-				<h2>Things to do<span>soon</span></h2>
+		<div className={styles.list}>
+			<header className={styles.header}>
+				<h2 className={styles.title}>Things to do<span>soon</span></h2>
 			</header>
-			<p>Interesting things I want to check out</p>
-			<section>
-				<article>
-					<h2>Books</h2>
-				</article>
-				<article>
-					<h2>Movies</h2>
-				</article>
-				<article>
-					<h2>Games</h2>
-				</article>
+			<p className={styles.description}>Interesting things I want to check out</p>
+			<section className={styles.columns}>
+				<Column title='Books' icon='book'/>
+				<Column title='Movies' icon='film'/>
+				<Column title='Games' icon='gamepad'/>
 			</section>
 		</div>
 	);
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,12 @@
+import styles from './SearchForm.module.scss'
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+
 const SearchForm = () => {
     return (
-        <form>
-            <input type="text" />
-            <button>Search</button>
+        <form className={styles.searchForm}>
+            <TextInput placeholder='Search…' />
+            <Button text='search'/>
         </form>
     );
   };
